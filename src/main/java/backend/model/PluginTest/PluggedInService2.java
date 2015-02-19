8175a6941792e4847dd397c8980e8c908a835c1d
Stix <stix@phcn.de>
2015-02-19 15:34:46 +0100

    trying cross plugin usage

--- src/main/java/backend/model/PluginTest/PluggedInService2.java
+++ src/main/java/backend/model/PluginTest/PluggedInService2.java
@@ -3,6 +3,7 @@ package backend.model.PluginTest;
 import ro.fortsoft.pf4j.Extension;
 import backend.model.result.SimpleResult;
 import backend.model.service.ServiceEntity;
+import backend.model.serviceprovider.GenericServiceProvider;
 
 @Extension
 public class PluggedInService2 extends ServiceEntity<SimpleResult>{
@@ -13,7 +14,13 @@ public class PluggedInService2 extends ServiceEntity<SimpleResult>{
 	}
 	@Override
 	public void execute() {
-		
+		try {
+			GenericServiceProvider provider = serviceProviderRepository().serviceProvider("b80b17302e4da7adc8599b9fb302a3b48cbee13e");
+			System.out.println(provider.commonName());
+		} catch (InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
